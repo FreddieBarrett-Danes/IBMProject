a8cda4fcc897829abf666a6f26f9d0cb8878a7cf
Its_A_Van <up2018426@myport.ac.uk>
2022-10-11 22:50:49 +0100

    Fixed Player Movement Script
    Fixed an issue to do with the player moving relative to it's own rotation not relative to vector3.up

--- IBM_Project/Assets/Programmer/Scripts/Player/Movement.cs
+++ IBM_Project/Assets/Programmer/Scripts/Player/Movement.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public float Speed;
+    public float speed;
     public GameObject player;
+    public GameObject visuals;
 
     private Vector3 move;
     public float rotationSpeed;
@@ -20,15 +21,18 @@ public class Movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        move = new Vector3(0, 0, 0);
+        //move = new Vector3(0, 0, 0);
         move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
-        this.gameObject.transform.Translate(move * Speed *Time.deltaTime);
+        this.gameObject.transform.Translate(move * speed * Time.deltaTime); 
 
-/*        if(move != Vector3.zero)
+        if(move != Vector3.zero)
         {
-            Quaternion toRotation = Quaternion.LookRotation(move, Vector3.up);
+            Vector3 NewAt = Vector3.Cross(transform.up, move);
 
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
-        }*/
+            Quaternion toRotation = new Quaternion();
+            toRotation.SetLookRotation(NewAt);
+            toRotation *= Quaternion.Euler(0, -90, 0);
+            visuals.transform.rotation = toRotation;
+        }
     }
 }
