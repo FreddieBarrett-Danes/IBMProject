9240764964ab51a3fde28770c3a9fa3f78a61dd9
FreddieBarrett-Danes <freddiebd@hotmail.com>
2022-10-11 14:25:07 +0100

    Normalize Movement

--- IBM_Project/Assets/Programmer/Scripts/Player/Movement.cs
+++ IBM_Project/Assets/Programmer/Scripts/Player/Movement.cs
@@ -1,51 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    public static Movement Instance;
     public float Speed;
     public GameObject player;
-//  public int Framerate; //Testing movement's consistant regardless of framerate
-    private Vector3 Origin;
-    Vector3 Distance;
+
+    private Vector3 move;
 	
 	
     // Start is called before the first frame update
     void Start()
     {
-        //   Application.targetFrameRate = Framerate;
-        //Timer = 0;
-        if (!Instance)
-        {
-            Instance = this;
-        }
 
-        //player = GameObject.FindGameObjectWithTag("Player");
-
-        Origin = player.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Using Input.GetKey
-        if (Input.GetKey("w"))
-        {
-            player.transform.position += new Vector3(0.0f, 0.0f, 0.1f) * (Time.deltaTime * Speed);
-        }
-        if (Input.GetKey("s"))
-        {
-            player.transform.position += new Vector3(0.0f, 0.0f, -0.1f) * (Time.deltaTime * Speed);
-        }
-        if (Input.GetKey("d"))
-        {
-            player.transform.position += new Vector3(0.1f, 0.0f, 0.0f) * (Time.deltaTime * Speed);
-        }
-        if (Input.GetKey("a"))
-        {
-            player.transform.position += new Vector3(-0.1f, 0.0f, 0.0f) * (Time.deltaTime * Speed);
-        }
+        move = new Vector3(0, 0, 0);
+        move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical")).normalized;
+        this.gameObject.transform.Translate(move * Speed *Time.deltaTime);
     }
 }
