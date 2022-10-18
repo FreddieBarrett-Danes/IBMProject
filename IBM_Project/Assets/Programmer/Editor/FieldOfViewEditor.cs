1901586eb682daae0cdf14d1609fd980aa3f271b
UP959339 <freddiebd@hotmail.com>
2022-10-19 00:03:40 +0100

    initial field of view script modified
    graphical issues still present

--- IBM_Project/Assets/Programmer/Editor/FieldOfViewEditor.cs
+++ IBM_Project/Assets/Programmer/Editor/FieldOfViewEditor.cs
@@ -16,9 +16,6 @@ public class FieldOfViewEditor : Editor {
 		Handles.DrawLine (fow.transform.position, fow.transform.position + viewAngleB * fow.viewRadius);
 
 		Handles.color = Color.red;
-		foreach (Transform visibleTarget in fow.visibleTargets) {
-			Handles.DrawLine (fow.transform.position, visibleTarget.position);
-		}
 	}
 
 }
