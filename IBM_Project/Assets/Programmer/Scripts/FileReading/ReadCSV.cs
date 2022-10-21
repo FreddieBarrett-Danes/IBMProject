9c6a8c4b2988685272ef1de28bddada670a606ec
Its_A_Van <up2018426@myport.ac.uk>
2022-10-22 00:17:09 +0100

    Fixed minor issues related to UI gen

--- IBM_Project/Assets/Programmer/Scripts/FileReading/ReadCSV.cs
+++ IBM_Project/Assets/Programmer/Scripts/FileReading/ReadCSV.cs
@@ -1,22 +1,20 @@
-using System;
+//using System;
 using System.Collections;
 using System.Collections.Generic;
-using System.Drawing;
-using System.Runtime.CompilerServices;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ReadCSV : MonoBehaviour
 {
-    public TextMeshProUGUI text;
+    public GameObject questionText;
+    public List<GameObject> answersList;
     public TextAsset CSVFile;
     private GameObject canvas;
     private RectTransform canvasRectTransform;
 
     public GameObject panelTest;
-    public List<GameObject> list;
-    public List<int> numbers;
     public Vector2 panelSize;
 
     private float startX;
@@ -24,64 +22,96 @@ public class ReadCSV : MonoBehaviour
 
     [Range(1, 5)]
     public int row;
-    [Range(1, 5)]
-    public int column;
 
     [SerializeField]
     private bool find; //Use this to generate the row,column that you've selected using Row and Column
+
+    List<int> Shuffle(int length)
+    {
+        List<int> orderedList = new List<int>(4);
+
+        for (int i = 1; i < length + 1; i++) //create list of numbers 1-4
+        {
+            orderedList.Add(i);
+        }
+
+        return orderedList;
+    }
+
+    public static List<int> FisherYatesShuffle(List<int> list)
+    {
+        System.Random sysRandom = new System.Random();
+
+        int tempInt;
+
+        int n = list.Count;
+        for (int i = 0; i < n; i++)
+        {
+            int r = i + (int)(sysRandom.NextDouble() * (n - i)); // NextDouble returns a random number between 0 and 1 (dont ask)
+            tempInt = list[r];
+            list[r] = list[i];
+            list[i] = tempInt;
+        }
+
+        return list;
+    }
+
+    string Find(int findRow, int findColmn)
+    {
+        string rv = null;
+
+        find = false;
+        var dataset = CSVFile;
+
+        var splitDataset = dataset.text.Split(new char[] { '\n' });
+
+        if (findRow < 1)
+        {
+            findRow = 1;
+            Debug.LogWarning("Desired Row given in the Find() function located on: " + this.gameObject.name + " was out of bounds. It was automatically brought back into range. - ask Istvan");
+        }
+
+        if (findColmn < 1)
+        {
+            findColmn = 1;
+            Debug.LogWarning("Desired Column given in the Find() function located on: " + this.gameObject.name + " was out of bounds. It was automatically brought back into range. - ask Istvan");
+        }
+
+        for (int i = 0; i < findRow; i++)
+        {
+            var data = splitDataset[i].Split(',');
+            for (int j = 0; j < findColmn; j++)
+            {
+                if (findRow > splitDataset.Length) findRow = splitDataset.Length;
+                if (findColmn > data.Length) findColmn = data.Length;
+
+                //questionText.text = data[j];
+
+                rv = data[j];
+            }
+        }
+        //Debug.Log(rv);
+        return rv;
+    }
 
     void Start()
     {
         canvas = GameObject.FindGameObjectWithTag("Canvas"); // may be ambiguous if theres several
         canvasRectTransform = canvas.GetComponent<RectTransform>();
-
-
     }
-
-    void RandomNumberSequence(int length)
-    {
-        int[] list;
-
-
-
-        return;
-    }
-
 
     void Update()
     {
         if (find) //File Reading / generate
         {
-            find = false;
-            var dataset = CSVFile;
-
-            var splitDataset = dataset.text.Split(new char[] { '\n' });
-
-            if (row <= 1) row = 1;
-            if (column <= 1) column = 1;
-
-            for (int i = 0; i < row; i++)
+            if (answersList.Count != 0) //Reset list
             {
-                var data = splitDataset[i].Split(',');
-                for (int j = 0; j < column; j++)
+                for (int i = 0; i < answersList.Count; i++)
                 {
-                    if (row > splitDataset.Length) row = splitDataset.Length;
-                    if (column > data.Length) column = data.Length;
-
-                    //Debug.Log(splitDataset.Length);
-                    text.text = data[j];
+                    Destroy(answersList[i]);
                 }
-            }
-
-            //Generate question answers
-
-            if (list.Count != 0) //Reset list
-            {
-                for (int i = 0; i < list.Count; i++)
-                {
-                    Destroy(list[i]);
-                }
-                list.Clear();
+                Destroy(questionText);
+                answersList.Clear();
             }
 
             startX = 0.25f; //Set start values for coords.
@@ -89,6 +119,17 @@ public class ReadCSV : MonoBehaviour
 
             panelSize = new Vector2((canvasRectTransform.sizeDelta.x * 0.9f) / 2, canvasRectTransform.sizeDelta.y * 0.25f); //Find panel size
 
+            //Instantiating question box
+
+            questionText = Instantiate(panelTest);
+            questionText.transform.SetParent(canvas.transform);
+            questionText.GetComponent<RectTransform>().sizeDelta = new Vector2(canvasRectTransform.sizeDelta.x * 0.95f, panelSize.y);
+            questionText.GetComponent<RectTransform>().position = new Vector2(canvasRectTransform.sizeDelta.x / 2, canvasRectTransform.sizeDelta.y * 0.83f);
+
+
+
+
+            //Instantiating answer boxes
             for (int i = 0; i < 2; i++)
             {
                 GameObject tempPanelY = Instantiate(panelTest);
@@ -97,7 +138,7 @@ public class ReadCSV : MonoBehaviour
 
                 tempPanelY.GetComponent<RectTransform>().position = new Vector2(canvasRectTransform.sizeDelta.x * startX, canvasRectTransform.sizeDelta.y * startY);
 
-                list.Add(tempPanelY);
+                answersList.Add(tempPanelY);
 
                 startX += 0.5f;
 
@@ -110,10 +151,10 @@ public class ReadCSV : MonoBehaviour
 
                     tempPanelZ.GetComponent<RectTransform>().position = new Vector2((canvasRectTransform.sizeDelta.x * startX), canvasRectTransform.sizeDelta.y * startY);
 
-                    list.Add(tempPanelZ);
+                    answersList.Add(tempPanelZ);
 
                     startX -= 0.5f;
-                    startY += 0.33f;
+                    startY += 0.28f; //was 0.33
                 }
             }
 
@@ -121,22 +162,15 @@ public class ReadCSV : MonoBehaviour
 
             panelTest.GetComponent<RectTransform>().position = new Vector2((canvasRectTransform.sizeDelta.x * startX), canvasRectTransform.sizeDelta.y * startY);
 
-            //Debug.Log(UnityEngine.Random.RandomRange(1,4));
+            List<int> orderList = new List<int>(FisherYatesShuffle(Shuffle(4)));
 
-            for (int k = 1; k < list.Count; k++)
+            for (int i = 0; i < answersList.Count; i++)
             {
-                int random = UnityEngine.Random.Range(0, list.Count);
-
-                if (random != numbers[k])
-                {
-                    //add
-
-                }
+                //ebug.Log(i);
+                answersList[i].GetComponentInChildren<TextMeshProUGUI>().text = Find(row, orderList[i] + 1).ToString();
             }
 
-            //numbers.Add(random);
-            //list[random].GetComponentInChildren<TextMeshProUGUI>().text = ("arrrrggghhhh");
-
+            questionText.GetComponentInChildren<TextMeshProUGUI>().text = Find(row, 1).ToString();
         }
     }
 }
