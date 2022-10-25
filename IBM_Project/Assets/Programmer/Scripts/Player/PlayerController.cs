550b09e728077e61654df6c532b247ee61f18dfb
FreddieBarrett-Danes <freddiebd@hotmail.com>
2022-10-25 15:43:54 +0100

    Prefab editing and collision script working

--- IBM_Project/Assets/Programmer/Scripts/Player/PlayerController.cs
+++ IBM_Project/Assets/Programmer/Scripts/Player/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private Rigidbody rBody;
     public Vector3 velocity;
 
-    public int threatLevel = 0;
+    public int threatLevel;
 
     //variables for shooting, only placed in here to test how dynamic functions are.
     private Shooting shooting;
