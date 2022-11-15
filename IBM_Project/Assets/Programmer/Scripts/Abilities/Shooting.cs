2ac6ebdc86029d34447683174f1dad9cf072bd84
FreddieBarrett-Danes <freddiebd@hotmail.com>
2022-11-15 13:20:27 +0000

    Added sprites to enemies in Prototype scene, bullet uses sprite now. Tilemap issue with dissapearing fixed

--- IBM_Project/Assets/Programmer/Scripts/Abilities/Shooting.cs
+++ IBM_Project/Assets/Programmer/Scripts/Abilities/Shooting.cs
@@ -10,8 +10,10 @@ public class Shooting : Ability
     {
         GameObject bullet = Resources.Load<GameObject>("Bullet");
         GameObject tempBullet = Instantiate(bullet, host.transform.position, host.transform.rotation);
+        
         Rigidbody tempRigidBodyBullet = tempBullet.GetComponent<Rigidbody>();
         tempRigidBodyBullet.AddForce(tempRigidBodyBullet.transform.forward * bulletSpeed);
+        tempBullet.transform.Rotate(90.0f, 0.0f, 0.0f, Space.Self);
         tempBullet.tag = host.tag;
         //Debug.Log(tempBullet.tag + " Bullet Shot");
         Destroy(tempBullet, 5f);
