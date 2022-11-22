d90974acc75121a95c8ec212fe5996f9376eb0de
UP2015264 <UP2015264@myport.ac.uk>
2022-11-22 14:18:10 +0000

    Materials for FOV

--- IBM_Project/Assets/Programmer/Scripts/Combat/BulletConditions.cs
+++ IBM_Project/Assets/Programmer/Scripts/Combat/BulletConditions.cs
@@ -1,45 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletConditions : MonoBehaviour
 {
     private GameObject[] enemies;
-    void Start()
+
+    private void Start()
     {
         enemies = GameObject.FindGameObjectsWithTag("Enemy");
     }
     private void OnTriggerEnter(Collider other)
     {
-       if(this.gameObject.tag == "Player")
+       if(gameObject.CompareTag("Player"))
        {
            foreach(GameObject enemy in enemies)
-            {
-                if (other.gameObject == enemy)
-                {
+           {
+               if (other.gameObject == enemy)
+               {
 
-                    Destroy(other.gameObject);
-                    Destroy(this.gameObject);
-                }
-                else if (other.gameObject == GameObject.FindGameObjectWithTag("Wall"))
-                {
-                    Destroy(this.gameObject);
-                }
-            }
+                   Destroy(other.gameObject);
+                   Destroy(gameObject);
+               }
+               else if (other.gameObject == GameObject.FindGameObjectWithTag("Wall"))
+               {
+                   Destroy(gameObject);
+               }
+           }
             
 
        }
-       else if (this.gameObject.tag == "Enemy")
+       else if (gameObject.CompareTag("Enemy"))
        {
            if (other.gameObject == GameObject.FindGameObjectWithTag("Player"))
-            {
+           {
                 Destroy(other.gameObject);
-                Destroy(this.gameObject);
-            }
-            else if (other.gameObject == GameObject.FindGameObjectWithTag("Wall"))
-            {
-                Destroy(this.gameObject);
-            }
+                Destroy(gameObject);
+           }
+           else if (other.gameObject == GameObject.FindGameObjectWithTag("Wall"))
+           {
+                Destroy(gameObject);
+           }
        }
         
         /*else
