43043dfe9b22bdbb2d26823c1877b0e907d834c8
Its_A_Van <up2018426@myport.ac.uk>
2023-01-24 14:30:13 +0000

    In Progress auto-sizing text

--- IBM_Project/Assets/Programmer/Scripts/FileReading/answersScript.cs
+++ IBM_Project/Assets/Programmer/Scripts/FileReading/answersScript.cs
@@ -43,6 +43,11 @@ public class answersScript : MonoBehaviour
         }*/
     }
 
+    void LateUpdate()
+    {
+        
+    }
+
     private void buttonClicked()
     {
         selected = !selected;
