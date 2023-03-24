2b510ce015deae8b851bb07dfe2eb47e17476ec7
UP2015264 <UP2015264@myport.ac.uk>
2023-03-24 14:25:19 +0000

    Death After Hack & Hack after Death

--- IBM_Project/Assets/Programmer/Scripts/Player/PlayerController.cs
+++ IBM_Project/Assets/Programmer/Scripts/Player/PlayerController.cs
@@ -135,6 +135,24 @@ public class PlayerController : MonoBehaviour
             controlTimer -= Time.deltaTime;
             if (controlTimer <= 0)
             {
+                GameObject tempBody;
+                switch (threatLevel)
+                {
+                    case 2:
+                    {
+                        Debug.Log("Solider_Death");
+                        tempBody = Resources.Load<GameObject>("Soldier_Death_Place");
+                        Instantiate(tempBody, transform.position, transform.parent.GetChild(2).rotation);
+                        break;
+                    }
+                    case 3:
+                    {
+                        Debug.Log("Scout_Death");
+                        tempBody = Resources.Load<GameObject>("Scout_Death_Place");
+                        Instantiate(tempBody, transform.position, transform.parent.GetChild(2).rotation);
+                        break;
+                    }
+                }
                 threatLevel = 0;
                 controlTimer = 0;
                 gameObject.GetComponent<Shooting>().enabled = false;
