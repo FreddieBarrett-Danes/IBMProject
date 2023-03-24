96b3042e9310204c3b702e46426a0f9862ed8480
UP959339 <freddiebd@hotmail.com>
2023-03-24 20:50:42 +0000

    bulet conditions

---
+++ IBM_Project/Assets/Programmer/Scripts/Combat/BulletConditions.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+
+public class BulletConditions : MonoBehaviour
+{
+    private GameObject[] enemies;
+    private GameController gC;
+    private AudioSource breakbox;
+
+    private void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag("EnemyScript");
+        gC = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+        breakbox = gameObject.GetComponent<AudioSource>();
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+       if(gameObject.CompareTag("Player"))
+       {
+           foreach(GameObject enemy in enemies)
+           {
+               if (other.gameObject == enemy)
+               {
+
+                   other.GetComponent<BotInfo>().bIsDead = true;
+                   Destroy(gameObject);
+               }
+               else if (other.gameObject.CompareTag("Wall"))
+               {
+                    Destroy(gameObject);
+
+               }
+               else if(other.gameObject.CompareTag("BreakableBox"))
+               {
+
+                    Destroy(other.gameObject);
+                    breakbox.enabled = true;
+
+                    
+                    //Destroy(gameObject);
+               }
+            }
+            
+
+       }
+       else if (gameObject.CompareTag("Enemy"))
+       {
+           if (other.gameObject.CompareTag("Player"))
+           {
+                //reduce level timer
+                gC.playerHit = true;
+                Destroy(gameObject);
+           }
+           else if (other.gameObject.CompareTag("Wall"))
+           {
+                Debug.Log("wall hit");
+                Destroy(gameObject);
+           }
+       }
+       else
+        {
+           if (other.gameObject.CompareTag("Wall"))
+           {
+
+                Destroy(gameObject);
+           }
+        }
+        
+        /*else
+        {
+            Destroy(this.gameObject);
+        }*/
+    }
+}
+
+
