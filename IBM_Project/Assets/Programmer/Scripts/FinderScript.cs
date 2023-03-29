4742e39aeb355e55fdda4227f97c4ffcd03ec19c
UP959339 <freddiebd@hotmail.com>
2023-03-29 20:07:58 +0100

    Update FinderScript.cs

--- IBM_Project/Assets/Programmer/Scripts/FinderScript.cs
+++ IBM_Project/Assets/Programmer/Scripts/FinderScript.cs
@@ -1,18 +1,1 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class FinderScript : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-}
+using UnityEngine; public class FinderScript : MonoBehaviour { /*LOL DO NOT REMOVE OR EDIT IE.  COCONUT.JPEG*/ }
